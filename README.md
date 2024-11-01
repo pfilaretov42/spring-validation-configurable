5ae0dfa0734e21209bcc683b46e4b9f99d6ea0a2
pfilaretov42 <petr.filaretov@gmail.com>
2024-11-01 08:18:37 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# spring-validation-configurable
+
+Spring Boot DTO validation with configurable constraint values.
+
+# How to run
+
---
+++ src/main/kotlin/dev/pfilaretov42/spring/validation/configurable/SpringValidationConfigurableApplication.kt
@@ -0,0 +1,11 @@
+package dev.pfilaretov42.spring.validation.configurable
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringValidationConfigurableApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringValidationConfigurableApplication>(*args)
+}
