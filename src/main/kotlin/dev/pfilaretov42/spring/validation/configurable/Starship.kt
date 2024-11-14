ba3a18a00e96d93ad9066a8fe532ec893bef53ce
pfilaretov42 <petr.filaretov@gmail.com>
2024-11-14 10:05:26 -0800

    Custom error message with arguments

--- src/main/kotlin/dev/pfilaretov42/spring/validation/configurable/Starship.kt
+++ src/main/kotlin/dev/pfilaretov42/spring/validation/configurable/Starship.kt
@@ -5,6 +5,6 @@ import org.hibernate.validator.constraints.Length
 
 class Starship(
     @field:NotBlank(message = "Starship name cannot be blank")
-    @field:Length(min = 10, max = 100)
+    @field:Length(min = 10, max = 100, message = "Starship name length must be between {min} and {max}")
     val name: String,
 )
